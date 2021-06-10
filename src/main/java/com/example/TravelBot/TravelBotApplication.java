3485b51f848cdf40159133872333ce88bf53df6a
Vlads <Vladsinitsa23@gmail.com>
2021-06-10 20:45:08 +0300

    Was set up BotConfig

--- src/main/java/com/example/TravelBot/TravelBotApplication.java
+++ src/main/java/com/example/TravelBot/TravelBotApplication.java
@@ -1,42 +1,37 @@
 package com.example.TravelBot;
 
-import com.example.TravelBot.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.telegram.telegrambots.ApiContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 @SpringBootApplication
-@EnableScheduling
 public class TravelBotApplication {
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TravelBotApplication.class, args);
+    @Bean
+    public TelegramBotsApi telegramBotsApi() {
+        return new TelegramBotsApi();
+    }
 
-		ApiContextInitializer.init();
+    @PostConstruct
+    public void init() {
+        ApiContextInitializer.init();
+    }
 
-		DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
-
-		BotConfig botConfig = new BotConfig(botOptions);
-
-		TelegramBotsApi botsApi = new TelegramBotsApi();
-		try {
-			botsApi.registerBot(botConfig);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TravelBotApplication.class, args);
+    }
 }
--- src/main/java/com/example/TravelBot/config/BotConfig.java
+++ src/main/java/com/example/TravelBot/config/BotConfig.java
@@ -1,45 +1,47 @@
 package com.example.TravelBot.config;
 
-import lombok.AccessLevel;
+import com.example.TravelBot.entity.CityEntity;
+import com.example.TravelBot.service.CityService;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
 @Getter
 @Setter
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@Component
 public class BotConfig extends TelegramLongPollingBot {
 
-    @Value("${telegrambot.webHookPath}")
-    String webHookPath;
-
     @Value("${telegrambot.botName}")
-    String botName;
+    private String botName;
 
     @Value("${telegrambot.botToken}")
-    String botToken;
+    private String botToken;
 
+    private final TelegramBotsApi telegramBotsApi;
+    private final CityService cityService;
 
-    public BotConfig(DefaultBotOptions options) {
-        super(options);
-    }
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        if(update.getMessage() != null && update.getMessage().hasText()){
-            long chatId = update.getMessage().getChatId();
-            try{
-                execute(new SendMessage(chatId, update.getMessage().getText()));
-            } catch (TelegramApiException e){
-                e.printStackTrace();
-            }
-        }
+    @Autowired
+    public BotConfig(TelegramBotsApi telegramBotsApi, CityService cityService) {
+        this.telegramBotsApi = telegramBotsApi;
+        this.cityService = cityService;
     }
 
     @Override
@@ -50,5 +52,83 @@ public class BotConfig extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return botName;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        String inMessage;
+        String chatId;
+
+        if (update.hasEditedMessage()) {
+            inMessage = update.getEditedMessage().getText();
+            chatId = update.getEditedMessage().getChatId().toString();
+        } else {
+            inMessage = update.getMessage().getText();
+            chatId = update.getMessage().getChatId().toString();
+        }
+
+        switch (inMessage) {
+            case "/start":
+                sendMessages(chatId, "Welcome to TravelBot!");
+                break;
+
+            case "You can view cities like:":
+                cityService.getAll().forEach(c -> sendMessages(chatId, String.valueOf(c)));
+                break;
+
+            case "Do you need help?":
+                sendMessages(chatId, "Write to me: Vladsinitsa23@gmail.com");
+                break;
+
+            default:
+                Optional<CityEntity> city = Optional.ofNullable(cityService.findByName(inMessage));
+                if (city.isPresent()) {
+                    sendMessages(chatId, String.valueOf(city.get()));
+                } else {
+                    sendMessages(chatId, "There is no information about the city: " + inMessage);
+                }
+                break;
+        }
+    }
+
+    private void sendMessages(final String chatId, final String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        setButtons(sendMessage);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Error during sending message.", e);
+        }
+    }
+
+    private void setButtons(final SendMessage sendMessage) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboard = new ArrayList<>();
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        keyboardFirstRow.add(new KeyboardButton("Cities"));
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        keyboardSecondRow.add(new KeyboardButton("Help"));
+
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    @PostConstruct
+    public void registryBot() {
+        try {
+            telegramBotsApi.registerBot(this);
+        } catch (TelegramApiRequestException e) {
+            log.error("Error occurred during bot registration.", e);
+        }
     }
 }
